2fb1c7b7d236d688d316a6f98fa0df153dc3a56b
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-05-07 10:43:55 -0500

    Prueba

---
+++ Backend-CaffeNet/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO admin (full_name, email, password, telefono) VALUES ("Admin", "caffenet.service@gmail.com", "e633f4fc79badea1dc5db970cf397c8248bac47cc3acf9915ba60b5d76b0e88f","31111111")
