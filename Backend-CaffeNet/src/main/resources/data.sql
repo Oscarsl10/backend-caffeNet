6fc7e73da6628c4bc1149185c016128e0ece3c23
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-05-10 21:43:11 -0500

    Cambios

--- Backend-CaffeNet/src/main/resources/data.sql
+++ Backend-CaffeNet/src/main/resources/data.sql
@@ -1,10 +1,3 @@
-INSERT INTO admin (full_name, email, password, telefono)
-VALUES
-  ('Admin',
-   'caffenet.service@gmail.com',
-   'e633f4fc79badea1dc5db970cf397c8248bac47cc3acf9915ba60b5d76b0e88f',
-   '31111111')
-ON DUPLICATE KEY UPDATE
-  full_name = VALUES(full_name),
-  password  = VALUES(password),
-  telefono  = VALUES(telefono);
+REPLACE INTO admin (full_name, email, password, telefono)
+VALUES ('Admin', 'caffenet.service@gmail.com',
+'e633f4fc79badea1dc5db970cf397c8248bac47cc3acf9915ba60b5d76b0e88f','31111111');
