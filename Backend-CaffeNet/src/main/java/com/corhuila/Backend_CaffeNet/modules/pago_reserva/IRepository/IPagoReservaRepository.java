445330089fe4e23c0bc3e35ccdf55b2052871af9
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-05-16 19:00:51 -0500

    Cambios

--- Backend-CaffeNet/src/main/java/com/corhuila/Backend_CaffeNet/modules/pago_reserva/IRepository/IPagoReservaRepository.java
+++ Backend-CaffeNet/src/main/java/com/corhuila/Backend_CaffeNet/modules/pago_reserva/IRepository/IPagoReservaRepository.java
@@ -2,9 +2,6 @@ package com.corhuila.Backend_CaffeNet.modules.pago_reserva.IRepository;
 
 import com.corhuila.Backend_CaffeNet.common.base.IBaseRepository;
 import com.corhuila.Backend_CaffeNet.modules.pago_reserva.Entity.PagoReserva;
-import com.corhuila.Backend_CaffeNet.modules.pedido.Entity.Pedido;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
